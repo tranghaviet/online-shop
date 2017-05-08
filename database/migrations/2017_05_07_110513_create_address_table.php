f94d3d74696458a61a1e2722b40d07c260df50c5
Tráng Hà Viết <trang12596@gmail.com>
2017-05-08 15:28:24 +0700

    address model

--- database/migrations/2017_05_07_110513_create_address_table.php
+++ database/migrations/2017_05_07_110513_create_address_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAddressTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id')->integer();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('userId');
+            $table->smallInteger('phone');
+            $table->string('state', 25);
+            $table->string('city', 25);
+            $table->string('address', 60);
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Shema::drop('address');
     }
 }
