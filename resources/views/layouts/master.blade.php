347c5357afabf58d9eba83f4a25cf8c77364b2a2
Tráng Hà Viết <trang12596@gmail.com>
2017-05-08 15:31:10 +0700

    master view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,30 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | Online Clothing Store</title>
-    <link href="{{asset('theme/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/prettyPhoto.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/price-range.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/animate.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/main.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/responsive.css')}}" rel="stylesheet">
-    <link href="{{asset('theme/css/jquery-ui.css')}}" rel="stylesheet">
+    <title>Online Clothing Store</title>
+    <link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/responsive.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/jquery-ui.css')}}" rel="stylesheet">
     <!--[if lt IE         9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->
-    <link rel="shortcut icon" href="{{asset('theme/images/ico/favicon.ico')}}">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144"
-          href="{{url('../')}}/theme/images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114"
-          href="{{url('../')}}/theme/images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72"
-          href="{{url('../')}}/theme/images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed"
-          href="{{url('../')}}/theme/images/ico/apple-touch-icon-57-precomposed.png">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="shortcut icon" href="{{asset('/images/ico/favicon.ico')}}">
+    <script src="{{asset('/js/jquery-1.12.4.js')}}"></script>
+    <script src="{{asset('/js/jquery-ui.js')}}"></script>
 
     <style>
         .brandLi {
@@ -54,6 +46,7 @@
     @include('includes.footer')
 </footer><!--/Footer-->
 
-<script src="{{asset('theme/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('/js/bootstrap.min.js')}}"></script>
+<a id="scrollUp" href="#top" style="position: fixed; z-index: 2147483647;"><i class="fa fa-angle-up"></i></a>
 </body>
 </html>
